e76c9fc0d8cc3b85cba59d9da6d5033aa2d5e95a
chetan sharan <chetan.sharan@rmoneyindia.com>
2025-01-06 11:28:33 +0530

    Changes in examples.

--- example/helper_function_examples/01_cm_master_to_csv.py
+++ example/helper_function_examples/01_cm_master_to_csv.py
@@ -21,8 +21,8 @@ root = API_root
 response_marketdata_login = xt_market_data.marketdata_login()
 
 market_data_get_master = xt_market_data.get_master(
-exchangeSegmentList = [xt_market_data.EXCHANGE_BSECM] # Works for BSECM as well.
+exchangeSegmentList = [xt_market_data.EXCHANGE_NSECM] # Works for BSECM as well.
 )
 
-(cm_master_string_to_df(market_data_get_master['result'])).to_excel("bse_cm_master.csv")
+(cm_master_string_to_df(market_data_get_master['result'])).to_csv("bse_cm_master.csv")
 """"""""""""""""""""""""""""""""""""""""""
